5f75f936176735da547168580648bfd2bcb0678d
Dustin Kline <klinetek@gmail.com>
2020-03-15 19:24:22 -0400

    You may now search the csv for transactions

---
+++ BillTracker.py
@@ -0,0 +1,5 @@
+import time
+import json
+
+
+class BillTracker:
--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -1,11 +1,11 @@
 
 text_file = open(r"transactions.csv", "r")
 lines = text_file.read().split('\n')
-totalSpent=0
-#date, vendor_name, type, empty, cost+/-
-for i in range(0, len(lines)):
-  if('CMSVEND*CV HARRISBURG MIDDLETOWN PA' in lines[i]):
-    tempStr=lines[i]
 
-    #totalSpent += double(lines[i])
-print(tempStr)
+totalSpent=0
+
+for i in range(0, len(lines)):
+  if('ALDI' in lines[i]):
+    tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
+    totalSpent += float(tempStr)
+print(totalSpent)
