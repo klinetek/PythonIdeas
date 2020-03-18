012deb8b5a39d3bde5f5e2a8858d42b6e012e9eb
Dustin Kline <klinetek@gmail.com>
2020-03-17 21:05:43 -0400

    going to do an interactive terminal thing!

--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -1,11 +1,11 @@
-
+def
 text_file = open(r"transactions.csv", "r")
 lines = text_file.read().split('\n')
 
 totalSpent=0
 
 for i in range(0, len(lines)):
-  if('CMSVEND' in lines[i]):
+  if('SUNOCO' in lines[i]): #do an str(input) for interactivity
     tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
     totalSpent += float(tempStr)
 print(totalSpent)
