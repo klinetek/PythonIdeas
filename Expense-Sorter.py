971bafc8c6d922281b19dd947357adab88289666
Dustin Kline <klinetek@gmail.com>
2020-03-28 17:17:45 -0400

    joe could be right

--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -5,14 +5,8 @@ fast_food = ['TACO', 'PAPA', 'DUNKIN', 'MCDONALD', 'YANGS', 'BURGER', 'STARBUCKS
 totalSpent=0
 
 for i in range(0, len(lines)):
-<<<<<<< HEAD
   if (0, len(fast_food[i])):
       for i in range (0, len(lines[i])):
           tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
           totalSpent += float(tempStr)
-=======
-  if('WAWA' in lines[i]): #do an str(input) for interactivity
-    tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
-    totalSpent += float(tempStr)
->>>>>>> 5461467be9bed31a60d990725033610557b42d55
 print(totalSpent)
