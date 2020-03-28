fca41aa7e9c34f45dd08c450a915455e5be2b085
Dustin Kline <klinetek@gmail.com>
2020-03-28 16:03:40 -0400

    it now runs everything for you.  ;) neat!

--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -1,12 +1,31 @@
-
 text_file = open(r"transactions.csv", "r")
 lines = text_file.read().split('\n')
-fast_food = ['TACO', 'PAPA', 'DUNKIN', 'MCDONALD', 'YANGS', 'BURGER', 'STARBUCKS', 'CHINA', 'LYNDON', 'IHOP']
-totalSpent=0
+
+fast_food = ['TACO', 'PAPA', 'DUNKIN', 'SONIC', 'MCDONALD', 'YANGS', 'BURGER', 'STARBUCKS', 'CHINA', 'LYNDON', 'IHOP', 'PIZZA']
+stores = ['MARSHALLS','AMAZON', 'DOLLAR', 'GOODWILL']
+Makeup = ['KYLIE', ' MERCARI', 'BEAUTY']
+
+fast_food_spent=0
+stores_spent = 0
+MakeupSpent = 0
 
 for i in range(0, len(lines)):
   for j in range(0, len(fast_food)):
       if (fast_food[j] in lines[i]):
           tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
-          totalSpent += float(tempStr)
-print(totalSpent)
+          fast_food_spent += float(tempStr)
+print("Fast Food " + str(fast_food_spent))
+
+for i in range(0, len(lines)):
+  for j in range(0, len(stores)):
+      if (stores[j] in lines[i]):
+          tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
+          stores_spent += float(tempStr)
+print("Stores " + str(stores_spent))
+
+for i in range(0, len(lines)):
+  for j in range(0, len(Makeup)):
+      if (Makeup[j] in lines[i]):
+          tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
+          MakeupSpent += float(tempStr)
+print("Makeup " + str(MakeupSpent))
