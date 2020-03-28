ab5f703c8dcbc07196fd4f9462c7c04e73341dd9
Dustin Kline <klinetek@gmail.com>
2020-03-28 17:12:04 -0400

    adding fast food list

--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -1,11 +1,12 @@
-def
+
 text_file = open(r"transactions.csv", "r")
 lines = text_file.read().split('\n')
-
+fast_food = ['TACO', 'PAPA', 'DUNKIN', 'MCDONALD', 'YANGS', 'BURGER', 'STARBUCKS', 'CHINA', 'LYNDON', 'IHOP']
 totalSpent=0
 
 for i in range(0, len(lines)):
-  if('SUNOCO' in lines[i]): #do an str(input) for interactivity
-    tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
-    totalSpent += float(tempStr)
+  if (0, len(fast_food[i])):
+      for i in range (0, len(lines[i])):
+          tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
+          totalSpent += float(tempStr)
 print(totalSpent)
