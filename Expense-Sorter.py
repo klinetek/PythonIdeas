437b79b252e8ff3a9ce6386425eaaba58bf50830
kline <klinetek@gmail.com>
2020-04-01 16:58:16 -0400

    fees included

--- Expense-Sorter.py
+++ Expense-Sorter.py
@@ -3,13 +3,21 @@ import timeit
 text_file = open(r"transactions.csv", "r")
 lines = text_file.read().split('\n')
 
-fast_food = ['TACO', 'PAPA', 'DUNKIN', 'SONIC', 'MCDONALD', 'YANGS', 'BURGER', 'STARBUCKS', 'CHINA', 'LYNDON', 'IHOP', 'PIZZA', 'COUSINS', 'EVANS']
+fast_food = ['TACO', 'PAPA', 'DUNKIN', 'SONIC', 'MCDONALD', 'YANGS', 'BURGER',
+            'STARBUCKS', 'CHINA', 'LYNDON', 'IHOP', 'PIZZA', 'COUSINS', 'EVANS']
 stores = ['MARSHALLS','AMAZON', 'DOLLAR', 'GOODWILL', 'FYE']
-Makeup = ['KYLIE', ' MERCARI', 'BEAUTY', 'COLOURPOP', 'MAC', "THE COSMETICS CO"]
+makeup = ['KYLIE', ' MERCARI', 'BEAUTY', 'COLOURPOP', 'MAC', "THE COSMETICS CO"]
 
 fast_food_spent=0
 stores_spent = 0
-MakeupSpent = 0
+makeup_spent = 0
+nsffeesspent = 0
+
+for i in range(0, len(lines)):
+     if('NSF' in lines[i]):
+         tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
+         nsffeesspent += float(tempStr)
+print("Fee's " + str(nsffeesspent))
 
 for i in range(0, len(lines)):
   for j in range(0, len(fast_food)):
@@ -26,11 +34,11 @@ for i in range(0, len(lines)):
 print("Stores " + str(stores_spent))
 
 for i in range(0, len(lines)):
-  for j in range(0, len(Makeup)):
-      if (Makeup[j] in lines[i]):
+  for j in range(0, len(makeup)):
+      if (makeup[j] in lines[i]):
           tempStr=lines[i][(int(lines[i].find('$'))+1):(int(lines[i].find(')'))-1)]
-          MakeupSpent += float(tempStr)
-print("Makeup " + str(MakeupSpent))
+          makeup_spent += float(tempStr)
+print("makeup " + str(makeup_spent))
 #break
 print("\tKyles Version ======================================================<3")
 #this version is harder on RAM because of all the splits
@@ -42,7 +50,7 @@ lines = text_file.read().split('\n')
 nlist = ["TACO,PAPA,DUNKIN,SONIC,MCDONALD,YANGS,BURGER,STARBUCKS,CHINA,LYNDON,IHOP,PIZZA,COUSINS,EVANS","MARSHALLS,AMAZON,DOLLAR,GOODWILL,FYE","KYLIE,MERCARI,BEAUTY,COLOURPOP,MAC,THECOSMETICSCO"]
 fast_food_spent=0
 stores_spent = 0
-MakeupSpent = 0
+makeup_spent = 0
 
 for i in range(0, len(lines)):
     for j in range(0, len(nlist)):
@@ -55,10 +63,10 @@ for i in range(0, len(lines)):
                 elif(j==1):
                     stores_spent += float(tempStr)
                 else:
-                    MakeupSpent += float(tempStr)
+                    makeup_spent += float(tempStr)
 print("Fast Food " + str(fast_food_spent))
 print("Stores " + str(stores_spent))
-print("Makeup " + str(MakeupSpent))
+print("makeup " + str(makeup_spent))
 #def main():
     #timeit.timeit(kyleIsAwsome, 100)
 
