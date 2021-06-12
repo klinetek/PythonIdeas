44bcccf2799193777cf575238989103156764711
Dustin Kline <klinetek@gmail.com>
2021-06-12 16:55:02 -0400

    f messages thanks to dylan! but the more efficient way

--- kyle_gets_this_much.py
+++ kyle_gets_this_much.py
@@ -1,6 +1,4 @@
 """You have worked for Sygma for {} years, you make ${} per hour, and you will make {}"""
-print("What was your hourly rate at Sygma?")
-dph = float(input())
-print("How many years did you work there?")
-years_worked = int(input())
-print("After working for {} years, making ${} per hour, you'll get ${} in severance".format(years_worked, dph, 240*dph))
+dph = float(input("What was your hourly rate at Sygma?"))
+years_worked = int(input("How many years did you work there?"))
+print(f"After working for {years_worked} years, making ${dph} per hour, you'll get ${240*dph} in severance")
